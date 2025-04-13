ca5ad8d1e30bdd2fd48dcfe78b379b54b909bd48
denni <dschwartz@stud.hs-heilbronn.de>
2025-04-13 11:58:40 +0200

    Aufsetzen von Spring Boot und Projektstruktur

---
+++ backend/Bücherei/src/main/java/com/example/Bucherei/BuchereiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bucherei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuchereiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuchereiApplication.class, args);
+	}
+
+}
---
+++ backend/Bücherei/src/test/java/com/example/Bucherei/BuchereiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bucherei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuchereiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
