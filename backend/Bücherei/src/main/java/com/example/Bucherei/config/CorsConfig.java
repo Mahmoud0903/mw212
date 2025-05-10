2abbda967df4d17beb2534b99d81673d5d7e4d08
Gundo12 <rumsbraten669@gmail.com>
2025-05-10 20:04:05 +0200

    added corsconfig

--- backend/Bücherei/src/main/java/com/example/Bucherei/config/CorsConfig.java
+++ backend/Bücherei/src/main/java/com/example/Bucherei/config/CorsConfig.java
@@ -14,9 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:8081") // oder Snowpack-Port
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*");
             }
         };
     }
