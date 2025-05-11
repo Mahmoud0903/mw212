621ba7be425c25e95ff10fbdeef595767c7fcdcb
Gundo12 <rumsbraten669@gmail.com>
2025-05-11 17:43:48 +0200

    a

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -3,7 +3,6 @@ import { User } from '../models/user';
 export async function fetchUser(id: number) {
   console.log(22);
   const response = await fetch(`http://localhost:8080/api/nutzer/${id}`);
-
   const user = await response.json();
 
   console.log(user);
